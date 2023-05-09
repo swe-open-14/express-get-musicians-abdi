68f11b3c455a508b9ad8392121c15b1056bd49ae
Abdi Majid <abdi.amajid.dev>
2023-05-09 15:28:47 +0100

    endpoints for musician and bands

--- index.test.js
+++ index.test.js
@@ -5,7 +5,7 @@ execSync('npm run seed');
 
 const request = require("supertest")
 const { db } = require('./db/connection');
-const { Musician } = require('./models/index')
+const { Musician,Band} = require('./models/index')
 const app = require('./src/app');
 const seedMusician = require("./seedData");
 
@@ -13,10 +13,52 @@ const seedMusician = require("./seedData");
 describe('./musicians endpoint', () => {
     // Write your tests here
     
+ test("Testing Musician endpoint", async () => {
+        // Sends request to `/bakedGoods` endpoint
+        const response = await request(app).get("/musicians");
+        expect(response.statusCode).toBe(200);
+    })  
+
+    test("Testing Musician Response is an array", async () => {
+        // Sends request to `/bakedGoods` endpoint
+        const response = await request(app).get("/musicians");
+        expect(Array.isArray(response.body)).toBe(true);
+    })  
     
+})
 
-
-
+describe('./musicians/1 endpoint', () => {
+    // Write your tests here
+    
+ test("Testing Musician/1 endpoint", async () => {
+        // Sends request to `/bakedGoods` endpoint
+        const response = await request(app).get("/musicians/1");
+        expect(response.statusCode).toBe(200);
+    })  
 
     
+    test("Testing Musician/1 Response name propertie", async () => {
+        // Sends request to `/bakedGoods` endpoint
+        const response = await request(app).get("/musicians/1");
+        expect(response.body.name).toBe("Mick Jagger");
+    })  
+
+})
+
+describe('./bands endpoint', () => {
+    // Write your tests here
+    
+ test("Testing bands endpoint", async () => {
+        // Sends request to `/bakedGoods` endpoint
+        const response = await request(app).get("/bands");
+        expect(response.statusCode).toBe(200);
+    })  
+
+    
+    test("Testing bands Response is an array", async () => {
+        // Sends request to `/bakedGoods` endpoint
+        const response = await request(app).get("/musicians");
+        expect(Array.isArray(response.body)).toBe(true);
+    })  
+
 })
--- src/app.js
+++ src/app.js
@@ -1,11 +1,41 @@
 const express = require("express");
 const app = express();
-const { Musician } = require("../models/index")
+const { Musician, Band } = require("../models/index")
 const { db } = require("../db/connection")
 
 const port = 3000;
 
 //TODO: Create a GET /musicians route to return all musicians 
+
+app.set("json spaces", 2);
+
+// TODO: Create your GET Request Route Below: 
+app.get("/musicians", async (req,res) =>{
+    try{
+const data = await Musician.findAll();
+res.json(data)
+    } catch (error) {
+        console.error(error);
+      }
+})
+
+app.get("/musicians/1", async (req,res) =>{
+    try{
+const data = await Musician.findAll();
+res.json(data[0])
+    } catch (error) {
+        console.error(error);
+      }
+})
+
+app.get("/bands", async (req,res) =>{
+    try{
+const data = await Band.findAll();
+res.json(data)
+    } catch (error) {
+        console.error(error);
+      }
+})
 
 
 
